27f48ba1aec5ff84d7343249658bf7a6874e16e3
Eder Leão Fernandes <ederleaofernandes@gmail.com>
2016-04-15 10:36:08 +0100

    Add test topology for umbrella
    This commit extends the test topology by adding a
    route server in order to test the isolated umbrella
    version.
    
    The RouteServer class inherits from the BGPRouter class
    and overrides the quaggaConfiguration method in order
    to add route server commands to the config file.

---
+++ examples/test-umbrella/Readme.md
@@ -0,0 +1,76 @@
+# Test Setup - MultiHop for Umbrella flows (large IXP setup)
+
+The test topology is a mesh network, with 4 edge switches connected to 4 core 
+switches, as illustrated below:
+
+![Test Topology](https://github.com/h2020-endeavour/endeavour/blob/master/examples/test-umbrella/large_ixp_edge_core_topology_visual.png)
+
+In this test there are 4 peering routers, running Quagga, named 
+**a1**, **b1**, **c1** and **c2**. These peers are configured to peer 
+with the fabric central route server named **x1**.
+
+Each router is a gateway for another 2 hosts:
+
+    a1 <- h1_a1, h2_a1
+    b1 <- h1_b1, h2_b1
+    c1 <- h1_c1, h2_c1
+    c2 <- h1_c2, h2_c2
+
+This setup considers that all participants peer between each other, so that, 
+after the initial flow setup, every host connected to a peer should be able to
+exchange packets.
+
+## Running the test
+
+Start the topology on Mininet.
+
+```bash
+$ cd endeavour && python examples/test-umbrella/mininet/simple_sdx.py
+```
+    
+In another terminal session.
+
+```bash
+$ ryu-manager umbrella/umbrella.py --config-file umbrella/fabric.cfg 
+```
+
+Flows and groups installed can be checked with ovs-ofctl. E.g:
+
+**Groups installed in the switch edge1 **
+```
+$ sudo ovs-ofctl -O OpenFlow13 dump-groups edge1
+OFPST_GROUP_DESC reply (OF1.3) (xid=0x2):
+group_id=4,type=ff,bucket=weight:0,watch_port:1,watch_group:0,actions=output:1,bucket=weight:0,watch_port:2,watch_group:0,actions=output:2,bucket=weight:0,watch_port:3,watch_group:0,actions=output:3,bucket=weight:0,watch_port:4,watch_group:0,actions=output:4
+group_id=1,type=ff,bucket=weight:0,watch_port:4,watch_group:0,actions=output:4,bucket=weight:0,watch_port:3,watch_group:0,actions=output:3,bucket=weight:0,watch_port:2,watch_group:0,actions=output:2,bucket=weight:0,watch_port:1,watch_group:0,actions=output:1
+group_id=2,type=ff,bucket=weight:0,watch_port:3,watch_group:0,actions=output:3,bucket=weight:0,watch_port:4,watch_group:0,actions=output:4,bucket=weight:0,watch_port:1,watch_group:0,actions=output:1,bucket=weight:0,watch_port:2,watch_group:0,actions=output:2
+group_id=3,type=ff,bucket=weight:0,watch_port:2,watch_group:0,actions=output:2,bucket=weight:0,watch_port:1,watch_group:0,actions=output:1,bucket=weight:0,watch_port:4,watch_group:0,actions=output:4,bucket=weight:0,watch_port:3,watch_group:0,actions=output:3
+```
+
+**Flows installed in the switch edge1**
+
+```
+$ sudo ovs-ofctl -O OpenFlow13 dump-flows edge1
+ OFPST_FLOW reply (OF1.3) (xid=0x2):
+ cookie=0x0, duration=106.971s, table=0, n_packets=214, n_bytes=16498, priority=1500,dl_dst=00:06:00:00:00:00/00:ff:00:00:00:00 actions=set_field:08:00:27:89:3b:ff->eth_dst,output:6
+ cookie=0x0, duration=106.971s, table=0, n_packets=0, n_bytes=0, priority=1500,dl_dst=00:05:00:00:00:00/00:ff:00:00:00:00 actions=set_field:08:00:27:89:3b:9f->eth_dst,output:5
+ cookie=0x0, duration=106.972s, table=0, n_packets=73, n_bytes=5705, priority=1000,dl_dst=08:00:27:92:18:1f actions=set_field:02:05:00:00:00:00->eth_dst,group:3
+ cookie=0x0, duration=106.973s, table=0, n_packets=54, n_bytes=4429, priority=1000,dl_dst=08:00:27:89:3b:ff actions=output:6
+ cookie=0x0, duration=106.971s, table=0, n_packets=76, n_bytes=5940, priority=1000,dl_dst=08:00:27:54:56:ea actions=set_field:03:05:00:00:00:00->eth_dst,group:4
+ cookie=0x0, duration=106.971s, table=0, n_packets=77, n_bytes=6006, priority=1000,dl_dst=08:00:27:bd:f8:b2 actions=set_field:04:05:00:00:00:00->eth_dst,group:1
+ cookie=0x0, duration=106.974s, table=0, n_packets=80, n_bytes=6186, priority=1000,dl_dst=08:00:27:89:3b:9f actions=output:5
+ cookie=0x0, duration=106.973s, table=0, n_packets=0, n_bytes=0, priority=1000,arp,arp_tpa=172.0.0.11,arp_op=1 actions=set_field:02:05:00:00:00:00->eth_dst,group:3
+ cookie=0x0, duration=106.971s, table=0, n_packets=1, n_bytes=42, priority=1000,arp,arp_tpa=172.0.0.21,arp_op=1 actions=set_field:03:05:00:00:00:00->eth_dst,group:4
+ cookie=0x0, duration=106.971s, table=0, n_packets=1, n_bytes=42, priority=1000,arp,arp_tpa=172.0.0.22,arp_op=1 actions=set_field:04:05:00:00:00:00->eth_dst,group:1
+ cookie=0x0, duration=106.973s, table=0, n_packets=1, n_bytes=42, priority=1000,arp,arp_tpa=172.0.255.254,arp_op=1 actions=output:6
+ cookie=0x0, duration=106.975s, table=0, n_packets=0, n_bytes=0, priority=1000,arp,arp_tpa=172.0.0.1,arp_op=1 actions=output:5
+```
+
+**Flows installed in the switch core1**
+```
+sudo ovs-ofctl -O OpenFlow13 dump-flows core1
+ OFPST_FLOW reply (OF1.3) (xid=0x2):
+ cookie=0x0, duration=730.778s, table=0, n_packets=478, n_bytes=36381, priority=1000,dl_dst=03:00:00:00:00:00/ff:00:00:00:00:00 actions=output:3
+ cookie=0x0, duration=730.778s, table=0, n_packets=0, n_bytes=0, priority=1000,dl_dst=01:00:00:00:00:00/ff:00:00:00:00:00 actions=output:1
+ cookie=0x0, duration=730.777s, table=0, n_packets=0, n_bytes=0, priority=1000,dl_dst=04:00:00:00:00:00/ff:00:00:00:00:00 actions=output:4
+ cookie=0x0, duration=730.778s, table=0, n_packets=0, n_bytes=0, priority=1000,dl_dst=02:00:00:00:00:00/ff:00:00:00:00:00 actions=output:2
+```
---
+++ examples/test-umbrella/mininet/simple_sdx.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.node import RemoteController, OVSSwitch, Node
+from sdnip import BgpRouter, SdnipHost, RouteServer
+
+ROUTE_SERVER_IP = '172.0.255.254'
+ROUTE_SERVER_ASN = 65000
+
+peers = [{'address': "172.0.0.1", 'as': "100"}, {'address': "172.0.0.11", 'as': "200"}, {'address': "172.0.0.21", 'as': "300"}, {'address': "172.0.0.22", 'as': "300"}]
+
+class SDXTopo( Topo ):
+    def __init__( self, *args, **kwargs ):
+        Topo.__init__( self, *args, **kwargs )
+        # Describe Code
+        # Set up data plane switch - this is the emulated router dataplane
+        # Note: The controller needs to be configured with the specific driver that
+        # will be attached to this switch.
+
+        # IXP fabric
+        # edge switches
+        edge_switches = []
+        for i in range(1, 5):
+            edge_switches.append(self.addSwitch(
+                'edge%s' % i, dpid='000000000000000%s' % i))
+
+        # core switches
+        core_switches = []
+        for i in range(1, 5):
+            core_switches.append(self.addSwitch(
+                'core%s' % i, dpid='00000000000000%s0' % i))
+
+        # connect edge to core links
+        for edge_switch in edge_switches:
+            for core_switch in core_switches:
+                self.addLink(edge_switch, core_switch)
+
+        # connect route server to edge 0 port 6
+        # route_server = self.addHost('x1', ip = '172.0.255.254/16', mac='08:00:27:89:3b:ff', inNamespace = False)
+        self.addRouteServer(fabric=edge_switches[0],
+                            name='x1',
+                            port=6,
+                            mac='08:00:27:89:3b:ff',
+                            ip='172.0.255.254/16',
+                            neighbors=peers,
+                            asn=65000)
+
+        # Add Participants to the IXP 
+        # Connects one participant to one of the four edge switches
+        # Each participant consists of 1 quagga router PLUS
+        # 1 host per network advertised behind quagga
+        self.addParticipant(fabric=edge_switches[0],
+                            name='a1',
+                            port=5,
+                            mac='08:00:27:89:3b:9f',
+                            ip='172.0.0.01/16',
+                            networks=['100.0.0.0/24', '110.0.0.0/24'],
+                            asn=100)
+
+        self.addParticipant(fabric=edge_switches[1],
+                            name='b1',
+                            port=5,
+                            mac='08:00:27:92:18:1f',
+                            ip='172.0.0.11/16',
+                            networks=['120.0.0.0/24', '130.0.0.0/24'],
+                            asn=200)
+        
+        self.addParticipant(fabric=edge_switches[2],
+                            name='c1',
+                            port=5,
+                            mac='08:00:27:54:56:ea',
+                            ip='172.0.0.21/16',
+                            networks=['140.0.0.0/24', '150.0.0.0/24'],
+                            asn=300)
+        
+        self.addParticipant(fabric=edge_switches[3],
+                            name='c2',
+                            port=5,
+                            mac='08:00:27:bd:f8:b2',
+                            ip='172.0.0.22/16',
+                            networks=['160.0.0.0/24', '170.0.0.0/24'],
+                            asn=300)
+
+    def addRouteServer(self,fabric,name,port,mac,ip,neighbors,asn):
+        # Adds the interface to connect the router to the peers
+        rseth0 = [{'mac': mac, 'ipAddrs': [ip]}]
+        intfs = {name+'-eth0': rseth0}
+
+        rs = self.addHost(name,
+                            intfDict=intfs,
+                            asNum=asn,
+                            neighbors=neighbors,
+                            cls=RouteServer)
+        self.addLink(fabric, rs, port)
+
+    def addParticipant(self,fabric,name,port,mac,ip,networks,asn):
+        # Adds the interface to connect the router to the Route server
+        peereth0 = [{'mac': mac, 'ipAddrs': [ip]}]
+        intfs = {name+'-eth0': peereth0}
+
+        # Set up the peer router
+        neighbors = [ {'address': ROUTE_SERVER_IP, 'as': ROUTE_SERVER_ASN}]
+
+        # Adds 1 gateway interface for each network connected to the router
+        for net in networks:
+            eth = {'ipAddrs': [replace_ip(net, '254')]}  # ex.: 100.0.0.254
+            i = len(intfs)
+            intfs[name + '-eth' + str(i)] = eth
+            
+        # Set up the peer router
+        neighbors = [{'address': ROUTE_SERVER_IP, 'as': ROUTE_SERVER_ASN}]
+        peer = self.addHost(name,
+                            intfDict=intfs,
+                            asNum=asn,
+                            neighbors=neighbors,
+                            routes=networks,
+                            cls=BgpRouter)
+        self.addLink(fabric, peer, port)
+        
+        # Adds a host connected to the router via the gateway interface
+        i = 0
+        for net in networks:
+            i += 1
+            ips = [replace_ip( net, '1' )]  # ex.: 100.0.0.1/24
+            hostname = 'h' + str(i) + '_' + name  # ex.: h1_a1
+            host = self.addHost(hostname ,
+                                cls=SdnipHost,
+                                ips=ips,
+                                gateway = replace_ip(net, '254').split('/')[0])  #ex.: 100.0.0.254
+            # Set up data plane connectivity
+            self.addLink( peer, host )
+
+def replace_ip(network, ip):
+    net,subnet=network.split('/')
+    gw=net.split('.')
+    gw[3]=ip
+    gw='.'.join(gw)
+    gw='/'.join([gw,subnet])
+    return gw
+
+if __name__ == "__main__":
+    setLogLevel('info')
+    topo = SDXTopo()
+
+    net = Mininet(topo=topo, controller=RemoteController, switch=OVSSwitch)
+
+    net.start()
+
+    CLI(net)
+
+    net.stop()
+
+    info("done\n")
