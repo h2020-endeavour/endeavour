529d396a788ac163df27578a07db120f9663b95e
ederlf <ederleaofernandes@gmail.com>
2016-04-22 17:55:40 +0100

    Add Umbrella app for iSDX global configuration.
    iSDX uses standalone apps to install flows. These apps
    communicate with the OpenFlow controller following
    a server client model.
    
    The main points implemented are:
        - Parsing of an iSDX like configuration file.
        - Creation of umbrella flows.
        - The idea of two new table identifiers for iSDX.
          (umbrella-edge and umbrella-core).
        - Initial abstract class for load balancing.
        - A dummy load balancing implementation.
    
    The load balancing is just a sketch and still need some
    decisions about the implementation design (e.g: how to work
    groups?)
    
    The code depends on some iSDX modules. For now it is considering
    the iSDX folder in the same level of endeavour.
    
    The code is able to comunicate and send the flow request to the iSDX
    reference monitor(basically, it is the iSDX app for the Ryu controller)
    Thus, it can be considered a first step towards the integration with iSDX.

---
+++ uctrl/lib.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import json
+import logging
+import os
+import sys
+from collections import namedtuple
+
+isdx_folder = "iSDX"
+home = os.path.expanduser("~/")
+isdx_path = home + isdx_folder
+if isdx_path not in sys.path:
+    sys.path.append(isdx_path)
+import util.log
+
+class Config(object):
+    def __init__(self, config_file):
+        # Connections between core and edge
+        self.core_edge = {}
+        self.edge_peers = {}
+        self.edge_to_edge = {}
+        self.edge_core = {}
+        self.participants = {}
+        self.refmon =  None
+        self.flanc_auth = None
+        self.route_server = None
+        config = json.load(open(config_file, 'r'))
+        
+        if "RefMon Server" in config:
+            self.refmon = config["RefMon Server"]
+
+        if "Flanc Auth Info" in config:
+            self.flanc_auth = config["Flanc Auth Info"]
+
+        if "Route Server" in config:
+            self.route_server = config["Route Server"]
+
+        if "Participants" in config:
+            self.participants = config["Participants"]
+        
+        if "RefMon Settings" in config:
+            if "fabric options" in config["RefMon Settings"]:
+                datapaths = config["RefMon Settings"]["fabric options"]["dpids"]
+                edges = {x:datapaths[x] for x in datapaths if x.find('edge') == 0}
+                cores = {x:datapaths[x] for x in datapaths if x.find('core') == 0}
+            if "fabric connections" in config["RefMon Settings"]:
+                datapaths_conns = config["RefMon Settings"]["fabric connections"]
+                for dp in edges:
+                    if dp in datapaths_conns:
+                        edge = edges[dp]
+                        self.parse_edge_peers(edge, self.participants, datapaths_conns[dp])
+                        self.parse_edge_core(edge, cores, datapaths_conns[dp])
+                        self.parse_edge_to_edge(edge, edges, datapaths_conns[dp])
+                for dp in cores:
+                    if dp in datapaths_conns:
+                        core = cores[dp]
+                        self.parse_core_edge(core, edges, datapaths_conns[dp])
+                    
+    # Code could be simpler with a different specification of configuration
+    # However, we want as few as possible modifications to iSDX
+    def parse_edge_peers(self, edge, participants, dp_conns):
+        self.edge_peers.setdefault(edge, {})
+        for conn in dp_conns:
+            # Only peer connections
+            if conn in participants:
+                ports = participants[conn]["Ports"]
+                # Eder: Would be interesting to have peers per ports in the 
+                # configuration file?
+                # peers = participants[conn]["Peers"]
+                for port in ports:
+                    ip = port["IP"]
+                    mac = port["MAC"]
+                    port = Port(mac, ip)
+                    self.edge_peers[edge][port] = dp_conns[conn]
+
+    def parse_edge_core(self, edge, cores, dp_conns):
+        self.edge_core.setdefault(edge, {})
+        for dp in dp_conns:
+            if dp in cores:
+                dpid = cores[dp]
+                self.edge_core[edge][dpid] = dp_conns[dp]
+
+    def parse_core_edge(self, core, edges, dp_conns):
+        self.core_edge.setdefault(core, {})
+        for dp in dp_conns:
+            if dp in edges:
+                dpid = edges[dp]
+                self.core_edge[core][dpid] = dp_conns[dp]
+
+    def parse_edge_to_edge(self, edge, edges, dp_conns):
+        self.edge_to_edge.setdefault(edge, {})
+        for dp in dp_conns:
+            if dp in edges:
+                self.edge_to_edge[edge][dp] = dp_conns[dp]
+
+Port = namedtuple('Peer', "mac ip")
---
+++ uctrl/load_balancer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import random
+
+# Not supposed to be something dynamic, for now.
+class Load_Balancer(object):
+    def __init__(self):
+        # Edge output ports in order of preference
+        # It will be calculated in the load_balance method
+        self.edge_out_ports = {}
+
+    def lb_policy(self, edge_core):
+        raise NotImplementedError
+
+    def lb_action(self, edge):
+        raise NotImplementedError
+
+class Dummy_LBalancer(Load_Balancer):
+    def __init__(self):
+        super(Dummy_LBalancer, self).__init__()
+
+    def lb_policy(self, edge_core):
+        for edge in edge_core:
+            self.edge_out_ports.setdefault(edge, {})
+            core = random.choice([x for x in edge_core[edge]])
+            self.edge_out_ports[edge] = (core, edge_core[edge][core])
+
+    def lb_action(self, edge):
+        return self.edge_out_ports[edge]
