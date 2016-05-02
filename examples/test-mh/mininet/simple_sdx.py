4c92d7c24657cc4e1fee33eee63e9296c4889a67
Sascha Bleidner <sascha.bleidner@de-cix.net>
2016-05-02 23:55:03 +0200

    Merged branch mn_config

--- examples/test-mh/mininet/simple_sdx.py
+++ examples/test-mh/mininet/simple_sdx.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+import argparse
+import os
+import sys
+
+sys.path.append('endeavour/uctrl/')
 
 from mininet.topo import Topo
 from mininet.net import Mininet
@@ -6,14 +11,16 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.node import RemoteController, OVSSwitch, Node
 from sdnip import BgpRouter, SdnipHost
+from lib import Config
 
 ROUTE_SERVER_IP = '172.0.255.254'
 ROUTE_SERVER_ASN = 65000
 
 
 class SDXTopo( Topo ):
-    def __init__( self, *args, **kwargs ):
+    def __init__( self, config, *args, **kwargs ):
         Topo.__init__( self, *args, **kwargs )
+        self.config = config
         # Describe Code
         # Set up data plane switch - this is the emulated router dataplane
         # Note: The controller needs to be configured with the specific driver that
@@ -38,59 +45,61 @@ class SDXTopo( Topo ):
                 self.addLink(edge_switch, core_switch)
 
 
-        # connect arp switch to edge 0 port 6       
+        # connect arp switch to edge 0 port 6
         arp_switch = self.addSwitch( 's2' , dpid='1000000000000000')
         self.addLink(edge_switches[0], arp_switch, 6, 1)
 
         # connect route server to edge 0 port 7
-        route_server = self.addHost('x1', ip = '172.0.255.254/16', mac='08:00:27:89:3b:ff', inNamespace = False)
+        route_server = self.addHost('x1', ip = self.config.route_server['IP'], mac=self.config.route_server['MAC'], inNamespace = False)
         self.addLink(edge_switches[0], route_server, 7)
-        
+
         # Add node for ARP Proxy"
-        arp_proxy = self.addHost('x2', ip = '172.0.255.253/16', mac='08:00:27:89:33:ff', inNamespace = False)
+        arp_proxy = self.addHost('x2', ip = self.config.arp_proxy['IP'], mac=self.config.arp_proxy['MAC'], inNamespace = False)
         self.addLink(arp_switch, arp_proxy, 2)
-        
 
-
-        # Add Participants to the IXP 
+        # Add Participants to the IXP
         # Connects one participant to one of the four edge switches
         # Each participant consists of 1 quagga router PLUS
         # 1 host per network advertised behind quagga
+        name = 'a1'
         self.addParticipant(fabric=edge_switches[0],
-                            name='a1',
-                            port=5,
-                            mac='08:00:27:89:3b:9f',
-                            ip='172.0.0.01/16',
+                            name=name,
+                            port=self.config.participants[name]['Ports'][0]['Id'],
+                            mac=self.config.participants[name]['Ports'][0]['MAC'],
+                            ip=self.config.participants[name]['Ports'][0]['IP'],
                             networks=['100.0.0.0/24', '110.0.0.0/24'],
-                            asn=100)
+                            asn=self.config.participants[name]['ASN'])
 
+        name = 'b1'
         self.addParticipant(fabric=edge_switches[1],
-                            name='b1',
-                            port=5,
-                            mac='08:00:27:92:18:1f',
-                            ip='172.0.0.11/16',
+                            name=name,
+                            port=self.config.participants[name]['Ports'][0]['Id'],
+                            mac=self.config.participants[name]['Ports'][0]['MAC'],
+                            ip=self.config.participants[name]['Ports'][0]['IP'],
                             networks=['140.0.0.0/24', '150.0.0.0/24'],
-                            asn=200)
-        
+                            asn=self.config.participants[name]['ASN'])
+
+        name = 'c1'
         self.addParticipant(fabric=edge_switches[2],
-                            name='c1',
-                            port=5,
-                            mac='08:00:27:54:56:ea',
-                            ip='172.0.0.21/16',
+                            name=name,
+                            port=self.config.participants[name]['Ports'][0]['Id'],
+                            mac=self.config.participants[name]['Ports'][0]['MAC'],
+                            ip=self.config.participants[name]['Ports'][0]['IP'],
                             networks=['140.0.0.0/24', '150.0.0.0/24'],
-                            asn=300)
-        
+                            asn=self.config.participants[name]['ASN'])
+
+        name = 'c2'
         self.addParticipant(fabric=edge_switches[3],
-                            name='c2',
-                            port=5,
-                            mac='08:00:27:bd:f8:b2',
-                            ip='172.0.0.22/16',
+                            name=name,
+                            port=self.config.participants[name]['Ports'][0]['Id'],
+                            mac=self.config.participants[name]['Ports'][0]['MAC'],
+                            ip=self.config.participants[name]['Ports'][0]['IP'],
                             networks=['140.0.0.0/24', '150.0.0.0/24'],
-                            asn=300)
+                            asn=self.config.participants[name]['ASN'])
 
 
 
-           
+
     def addParticipant(self,fabric,name,port,mac,ip,networks,asn):
         # Adds the interface to connect the router to the Route server
         peereth0 = [{'mac': mac, 'ipAddrs': [ip]}]
@@ -101,7 +110,7 @@ class SDXTopo( Topo ):
             eth = {'ipAddrs': [replace_ip(net, '254')]}  # ex.: 100.0.0.254
             i = len(intfs)
             intfs[name + '-eth' + str(i)] = eth
-            
+
         # Set up the peer router
         neighbors = [{'address': ROUTE_SERVER_IP, 'as': ROUTE_SERVER_ASN}]
         peer = self.addHost(name,
@@ -111,7 +120,7 @@ class SDXTopo( Topo ):
                             routes=networks,
                             cls=BgpRouter)
         self.addLink(fabric, peer, port)
-        
+
         # Adds a host connected to the router via the gateway interface
         i = 0
         for net in networks:
@@ -135,7 +144,17 @@ def replace_ip(network, ip):
 
 if __name__ == "__main__":
     setLogLevel('info')
-    topo = SDXTopo()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', help='path of config file')
+    args = parser.parse_args()
+
+    # locate config file
+    config_file = os.path.abspath(args.config)
+
+    config = Config(config_file)
+
+    topo = SDXTopo(config)
 
     net = Mininet(topo=topo, controller=RemoteController, switch=OVSSwitch)
 
--- uctrl/lib.py
+++ uctrl/lib.py
@@ -11,7 +11,7 @@ home = os.path.expanduser("~/")
 isdx_path = home + isdx_folder
 if isdx_path not in sys.path:
     sys.path.append(isdx_path)
-import util.log
+#import util.log
 
 class Config(object):
     def __init__(self, config_file):
@@ -24,8 +24,9 @@ class Config(object):
         self.refmon =  None
         self.flanc_auth = None
         self.route_server = None
+        self.arp_proxy = None
         config = json.load(open(config_file, 'r'))
-        
+
         if "RefMon Server" in config:
             self.refmon = config["RefMon Server"]
 
@@ -35,9 +36,12 @@ class Config(object):
         if "Route Server" in config:
             self.route_server = config["Route Server"]
 
+        if "ARP Proxy" in config:
+            self.arp_proxy = config["ARP Proxy"]
+
         if "Participants" in config:
             self.participants = config["Participants"]
-        
+
         if "RefMon Settings" in config:
             if "fabric options" in config["RefMon Settings"]:
                 datapaths = config["RefMon Settings"]["fabric options"]["dpids"]
@@ -55,7 +59,7 @@ class Config(object):
                     if dp in datapaths_conns:
                         core = cores[dp]
                         self.parse_core_edge(core, edges, datapaths_conns[dp])
-                    
+
     # Code could be simpler with a different specification of configuration
     # However, we want as few as possible modifications to iSDX
     def parse_edge_peers(self, edge, participants, dp_conns):
@@ -64,7 +68,7 @@ class Config(object):
             # Only peer connections
             if conn in participants:
                 ports = participants[conn]["Ports"]
-                # Eder: Would be interesting to have peers per ports in the 
+                # Eder: Would be interesting to have peers per ports in the
                 # configuration file?
                 # peers = participants[conn]["Peers"]
                 for port in ports:
@@ -73,6 +77,9 @@ class Config(object):
                     port = Port(mac, ip)
                     self.edge_peers[edge][port] = dp_conns[conn]
 
+
+    # Builds a list with:
+    # edge dp id - core dp id -> port
     def parse_edge_core(self, edge, cores, dp_conns):
         self.edge_core.setdefault(edge, {})
         for dp in dp_conns:
