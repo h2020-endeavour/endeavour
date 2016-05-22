25b063c3783bc218ba607ed3a2933a5a80fd6279
Sascha Bleidner <sascha.bleidner@de-cix.net>
2016-05-22 21:51:51 +0200

    Merge mininet#18

--- examples/test-mh/mininet/simple_sdx.py
+++ examples/test-mh/mininet/simple_sdx.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import argparse
-import os, sys
+import os
+import sys
 import json
 
-sys.path.append('endeavour/uctrl')
+sys.path.append('/home/vagrant/endeavour/uctrl')
 from lib import Config
 
 from mininet.topo import Topo
@@ -14,14 +15,13 @@ from mininet.node import RemoteController, OVSSwitch, Node
 from sdnip import BgpRouter, SdnipHost
 from lib import Config
 
-ROUTE_SERVER_IP = '172.0.255.254'
-ROUTE_SERVER_ASN = 65000
+path_mininet_config = '/home/vagrant/endeavour/examples/test-mh/mininet/mininet.cfg'
 
-path_mininet_config = 'endeavour/examples/test-mh/mininet/mininet.cfg'
 
-class SDXTopo( Topo ):
-    def __init__( self, config, *args, **kwargs ):
-        Topo.__init__( self, *args, **kwargs )
+class SDXTopo(Topo):
+
+    def __init__(self, config, *args, **kwargs):
+        Topo.__init__(self, *args, **kwargs)
         self.config = config
         # Describe Code
         # Set up data plane switch - this is the emulated router dataplane
@@ -34,57 +34,58 @@ class SDXTopo( Topo ):
 
         participant_config = json.load(open(path_mininet_config, 'r'))
 
-        for i in range(1, 5):
+        for i in range(1, len(filter(lambda s: 'edge' in str(s), config.dpids))+1):
             edge_switches.append(self.addSwitch(
-                'edge%s' % i, dpid='000000000000000%s' % i))
+                'edge-%s' % i, dpid=format(config.dpids['edge-%s' % i], '016x')))
 
         # core switches
         core_switches = []
-        for i in range(1, 5):
+        for i in range(1, len(filter(lambda s: 'core' in str(s), config.dpids))+1):
             core_switches.append(self.addSwitch(
-                'core%s' % i, dpid='00000000000000%s0' % i))
+                'core-%s' % i, dpid=format(config.dpids['core-%s' % i], '016x')))
 
         # connect edge to core links
         for edge_switch in edge_switches:
             for core_switch in core_switches:
                 self.addLink(edge_switch, core_switch)
 
-
         # connect arp switch to edge 0 port 6
-        arp_switch = self.addSwitch( 's2' , dpid='1000000000000000')
+        arp_switch = self.addSwitch('s2', dpid=format(config.dpids['arp-switch'], '016x'))
         self.addLink(edge_switches[0], arp_switch, 6, 1)
 
         # connect route server to edge 0 port 7
-        route_server = self.addHost('x1', ip = self.config.route_server['IP'], mac=self.config.route_server['MAC'], inNamespace = False)
+        route_server = self.addHost('x1', ip=self.config.route_server.ip, mac=self.config.route_server.mac, inNamespace=False)
         self.addLink(edge_switches[0], route_server, 7)
 
+
+
         # Add node for ARP Proxy"
-        arp_proxy = self.addHost('x2', ip = self.config.arp_proxy['IP'], mac=self.config.arp_proxy['MAC'], inNamespace = False)
+        arp_proxy = self.addHost('x2', ip=self.config.arp_proxy.ip, mac=self.config.arp_proxy.mac, inNamespace=False)
         self.addLink(arp_switch, arp_proxy, 2)
 
         # Add Participants to the IXP
         # Connects one participant to one of the four edge switches
         # Each participant consists of 1 quagga router PLUS
         # 1 host per network advertised behind quagga
-        
+ 
         name = 'a1'
         self.addParticipant(fabric=edge_switches[0],
-                            name=name,
+                             name=name,
                             port=participant_config[name]['port'],
                             mac=participant_config[name]['mac'],
                             ip=participant_config[name]['ip'],
                             networks=participant_config[name]['networks'],
                             asn=participant_config[name]['asn'])
-        
+
         name = 'b1'
         self.addParticipant(fabric=edge_switches[1],
-                            name=name,
+                             name=name,
                             port=participant_config[name]['port'],
                             mac=participant_config[name]['mac'],
                             ip=participant_config[name]['ip'],
                             networks=participant_config[name]['networks'],
                             asn=participant_config[name]['asn'])
-        
+
         name = 'c1'
         self.addParticipant(fabric=edge_switches[2],
                              name=name,
@@ -96,7 +97,7 @@ class SDXTopo( Topo ):
 
         name = 'c2'
         self.addParticipant(fabric=edge_switches[3],
-                            name=name,
+                             name=name,
                             port=participant_config[name]['port'],
                             mac=participant_config[name]['mac'],
                             ip=participant_config[name]['ip'],
@@ -104,10 +105,10 @@ class SDXTopo( Topo ):
                             asn=participant_config[name]['asn'])
 
 
-    def addParticipant(self,fabric,name,port,mac,ip,networks,asn):
+    def addParticipant(self, fabric, name, port, mac, ip, networks, asn):
         # Adds the interface to connect the router to the Route server
         peereth0 = [{'mac': mac, 'ipAddrs': [ip]}]
-        intfs = {name+'-eth0': peereth0}
+        intfs = {name + '-eth0': peereth0}
 
         # Adds 1 gateway interface for each network connected to the router
         for net in networks:
@@ -116,7 +117,7 @@ class SDXTopo( Topo ):
             intfs[name + '-eth' + str(i)] = eth
 
         # Set up the peer router
-        neighbors = [{'address': ROUTE_SERVER_IP, 'as': ROUTE_SERVER_ASN}]
+        neighbors = [{'address': self.config.route_server.ip, 'as': self.config.route_server.asn}]
         peer = self.addHost(name,
                             intfDict=intfs,
                             asNum=asn,
@@ -129,21 +130,22 @@ class SDXTopo( Topo ):
         i = 0
         for net in networks:
             i += 1
-            ips = [replace_ip( net, '1' )]  # ex.: 100.0.0.1/24
+            ips = [replace_ip(net, '1')]  # ex.: 100.0.0.1/24
             hostname = 'h' + str(i) + '_' + name  # ex.: h1_a1
-            host = self.addHost(hostname ,
+            host = self.addHost(hostname,
                                 cls=SdnipHost,
                                 ips=ips,
-                                gateway = replace_ip(net, '254').split('/')[0])  #ex.: 100.0.0.254
+                                gateway=replace_ip(net, '254').split('/')[0])  # ex.: 100.0.0.254
             # Set up data plane connectivity
-            self.addLink( peer, host )
+            self.addLink(peer, host)
+
 
 def replace_ip(network, ip):
-    net,subnet=network.split('/')
-    gw=net.split('.')
-    gw[3]=ip
-    gw='.'.join(gw)
-    gw='/'.join([gw,subnet])
+    net, subnet = network.split('/')
+    gw = net.split('.')
+    gw[3] = ip
+    gw = '.'.join(gw)
+    gw = '/'.join([gw, subnet])
     return gw
 
 if __name__ == "__main__":
--- uctrl/lib.py
+++ uctrl/lib.py
@@ -41,7 +41,7 @@ class Config(object):
 
         if "Route Server" in config:
             route_server = config["Route Server"]
-            self.route_server = Port(route_server['Port'], route_server["MAC"], route_server["IP"], route_server["switch"])
+            self.route_server = RS(route_server['Port'], route_server["MAC"], route_server["IP"], route_server["switch"], route_server["ASN"])
 
         if "ARP Proxy" in config:
             arp_proxy = config["ARP Proxy"]
@@ -98,7 +98,7 @@ class Config(object):
                             dpid = self.dpids[dp]
                             self.edge_peers[dpid][port] = ports[i]
                             i += 1
-                            print self.edge_peers
+                            #print self.edge_peers
 
     # Builds a list with:
     # edge dp id - core dp id -> port
@@ -121,5 +121,6 @@ class Config(object):
         for dp in dp_conns:
             if dp in edges:
                 self.edge_to_edge[edge][dp] = dp_conns[dp]
-
+ 
 Port = namedtuple('Port', "id mac ip switch")
+RS = namedtuple('RS', "id mac ip switch asn")
