9cdc0739e7d492008e3bd016ab7c00d7ee5d9a6c
Florian Kaufmann <florian.kaufmann@de-cix.net>
2016-05-23 17:14:46 +0200

    change ip_lbalancer match

--- uctrl/load_balancer.py
+++ uctrl/load_balancer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import random
+from ryu.ofproto import parser
 
 # Not supposed to be something dynamic, for now.
 class Load_Balancer(object):
@@ -34,6 +35,9 @@ class IP_LBalancer(Load_Balancer):
     def __init__(self, config):
         super(IP_LBalancer, self).__init__()
 
+
+
+
     def lb_policy(self, edge_core):
         for edge in edge_core:
             print "edge_core: %s" % edge_core
@@ -43,3 +47,22 @@ class IP_LBalancer(Load_Balancer):
 
     def lb_action(self, edge):
         return self.edge_out_ports[edge]
+
+
+    def generate_load_balancing_matches(self, config):
+
+        datapaths = config.datapaths
+        cores = [datapaths[x] for x in datapaths if x.find("core") == 0]
+
+        metadata = []
+        for core in cores:
+            metadata.append(core.id)
+
+        # test matches, to be extended
+        match0 = parser.OFPMatch(eth_type=0x0800, ipv4_src=('1.0.0.0', '192.0.0.0'))
+        match1 = parser.OFPMatch(eth_type=0x0800, ipv4_src=('64.0.0.0', '192.0.0.0'))
+        match2 = parser.OFPMatch(eth_type=0x0800, ipv4_src=('128.0.0.0', '192.0.0.0'))
+        match3 = parser.OFPMatch(eth_type=0x0800, ipv4_src=('192.0.0.0', '192.0.0.0'))
+        matches = [match0, match1, match2, match3]
+
+        return matches, metadata
