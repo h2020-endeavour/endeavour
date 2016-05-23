7e7b17aafd1d354372a3f2b291205ea86eac2a63
Florian Kaufmann <florian.kaufmann@de-cix.net>
2016-05-23 17:54:30 +0200

    add match to all networks in umbrella for ip_lbalancer

--- uctrl/umbrella.py
+++ uctrl/umbrella.py
@@ -143,8 +143,9 @@ class Umbrella(object):
             ipv4_src=('64.0.0.0', '192.0.0.0')
         elif net_id == 3:
             ipv4_src=('128.0.0.0', '192.0.0.0')
-        else net_id == 4:
+        elif net_id == 4:
             ipv4_src=('192.0.0.0', '192.0.0.0')
+        else ipv4_src=('1.0.0.0', '0.0.0.0')
         match = {"eth_type": ETH_TYPE_IP, "ipv4_src": ipv4_src}
         return match
 
