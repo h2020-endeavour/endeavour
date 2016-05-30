5eeeca9eea5b61888c8906c63e578f13f7b70901
Florian Kaufmann <florian.kaufmann@de-cix.net>
2016-05-30 10:53:58 +0200

    add debug information for action output port

--- uctrl/umbrella.py
+++ uctrl/umbrella.py
@@ -172,6 +172,8 @@ class Umbrella(object):
                 
                 match = self.ip_match(core_id)
                 
+                print "core_id: %s and edge: %s" % core_id, edge 
+
                 out_port = self.config.core_edge[core_id][edge]
                 action = {"fwd": [out_port]} # make new action!! TODO
                 
