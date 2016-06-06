e5efe54b4ec84c2e5385ee5b05216967ed3d2d70
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2016-06-06 10:47:04 +0200

    add parameter

--- tm-launch.sh
+++ tm-launch.sh
@@ -6,9 +6,9 @@ session_name="endeavour"
 
 tmux new-session -d -s $session_name
 
-tmux new-window -t $session_name:1 -n 'logserver' "$RUN_DIR/launch.sh test-mh 1"
-tmux new-window -t $session_name:2 -n 'mininet' "$RUN_DIR/launch.sh test-mh 2"
-tmux new-window -t $session_name:3 -n 'everythingElse' "$RUN_DIR/launch.sh test-mh 3"
+tmux new-window -t $session_name:1 -n 'logserver' "$RUN_DIR/launch.sh $1 1"
+tmux new-window -t $session_name:2 -n 'mininet' "$RUN_DIR/launch.sh $1 2"
+tmux new-window -t $session_name:3 -n 'everythingElse' "$RUN_DIR/launch.sh $1 3"
 
 tmux select-window -t $session_name:3
 tmux attach-session -t $session_name
