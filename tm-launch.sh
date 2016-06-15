5f82a1460a683b7ac5f0acc32eb93773c0645f7e
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2016-06-15 13:00:16 +0200

    increase the sleeping time

--- tm-launch.sh
+++ tm-launch.sh
@@ -14,7 +14,7 @@ tmux new-session -d -s $session_name
 tmux new-window -t $session_name:1 -n 'logserver' "$RUN_DIR/launch.sh $1 1"
 sleep 2
 tmux new-window -t $session_name:2 -n 'mininet' "$RUN_DIR/launch.sh $1 2"
-sleep 2
+sleep 5
 tmux new-window -t $session_name:3 -n 'everythingElse' "$RUN_DIR/launch.sh $1 3"
 
 tmux select-window -t $session_name:3
