b788aa0559c1fa46a49b10b2f827cb6dc9f02a41
Marco Canini <marco.canini@uclouvain.be>
2016-06-15 17:07:38 +0200

    Add launch script

---
+++ tm-launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+RUN_DIR=~/endeavour
+SDX_DIR=~/iSDX
+RIBS_DIR=$SDX_DIR/xrs/ribs
+session_name="endeavour"
+
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied"
+    exit 1
+fi
+
+tmux new-session -d -s $session_name
+
+tmux new-window -t $session_name:1 -n 'logserver' "$RUN_DIR/launch.sh $1 1"
+sleep 2
+tmux new-window -t $session_name:2 -n 'mininet' "$RUN_DIR/launch.sh $1 2"
+sleep 10
+tmux new-window -t $session_name:3 -n 'everythingElse' "$RUN_DIR/launch.sh $1 3"
+
+tmux select-window -t $session_name:3
+tmux attach-session -t $session_name
