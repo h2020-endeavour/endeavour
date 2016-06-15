b238eebf93eaa09ac8731fb8875f6b97a8c93f68
Florian Kaufmann <florian.kaufmann@de-cix.net>
2016-06-15 11:03:43 +0200

    fix another bug

--- uctrl/load_balancer.py
+++ uctrl/load_balancer.py
@@ -86,7 +86,6 @@ class IP_LBalancer(Load_Balancer):
         for core in self.config.cores:
         # link every core to a match
         # works only for the 4. byte
-        for core in cores:
             core_id = self.config.cores[core]
             set_elem = setarray[3] # fix atm look only at byte 4
             elem = set_elem.pop()
