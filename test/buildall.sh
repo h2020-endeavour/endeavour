08abd0da195ff34dd468c9ba47fb710b4700772c
Marco Canini <marco@dhcp-21-83.laas.fr>
2016-06-30 10:39:57 +0200

    Ensure buildall makes a link to the test files in the iSDX directory.

--- test/buildall.sh
+++ test/buildall.sh
@@ -1,3 +1,6 @@
+BASE=~/endeavour
+BASE_ISDX=~/iSDX
+
 mkdir output
 for i in specs/*.spec
 do
@@ -7,4 +10,5 @@ do
 	rm -rf output/$file
 	python gen_test.py $i
 	mv output/$file ../examples
+	ln -s $BASE/examples/$file $BASE_ISDX/examples/$file
 done
