5b34a96cf355726105d7a9d8efca2ecee8f08729
Florian Kaufmann <florian.kaufmann@de-cix.net>
2016-08-29 17:06:56 +0200

    update participant_client for blackholing_demo

--- pclnt/participant_client.py
+++ pclnt/participant_client.py
@@ -30,20 +30,36 @@ class ParticipantClient(object):
         self.cfg = PConfig(config_file, self.id)
         self.logger = logger
 
+    def file_handler(self, policy_file, id_list):    
+        add_policy = ''
+        print id_list
+        id_list = ['4096']
+        # Open File and Parse
+        with open(policy_file, 'r') as f:
+            policies=json.load(f)
 
-    def send(self, policy_file, action):
+            for policy in policies['inbound']:
+                if policy['cookie'] in id_list:
+                    print True
+                    add_policy+=str(json.dumps(policy))
+
+        data = ('{ "inbound" : [ %s ] }' % add_policy)
+        return data
+
+{u'policy': [{u'insert': {u'inbound': [{u'action': {u'drop': 0}, u'cookie': u'4096', u'match': {u'eth_src': u'08:00:bb:bb:01:00'}}]}}]}
+
+    def process_handler(self, policy_file, action, id_list):
 
         # Connect to participant client
         self.client = self.cfg.get_participant_client(self.id, self.logger)
         
-        # Open File and Parse
-        with open(policy_file, 'r') as f:
-            raw_data=f.read()
-            data = json.loads('{ "policy": [ { "%s": [ %s ] } ] }' % (action, raw_data))
-
+        # Get data from file
+        data = json.loads('{ "policy": [ { "%s": %s } ] }' % (action, self.file_handler(policy_file, id_list)))
+        print data
         # Send data
         self.logger.debug("participant_client(%s): send: %s" % (self.id, data))
         self.client.send(data)
+
 
 
     def stop(self):
@@ -62,6 +78,7 @@ def main():
     parser.add_argument('policy_file', help='the policy change file')
     parser.add_argument('id', type=int, help='participant id (integer)')
     parser.add_argument('action', type=str, choices=valid_actions, help='use remove or insert')
+    parser.add_argument('id_list', type=str, nargs='?', help='list of cookies/ids')
     args = parser.parse_args()
 
     # locate policy changefile
@@ -78,7 +95,7 @@ def main():
 
     # start controller
     prtclnt = ParticipantClient(args.id, config_file, logger)
-    prtclnt_thread = Thread(target=prtclnt.send(policy_file, args.action))
+    prtclnt_thread = Thread(target=prtclnt.process_handler(policy_file, args.action, args.id_list.split(',')))
     prtclnt_thread.daemon = True
     prtclnt_thread.start()
 
